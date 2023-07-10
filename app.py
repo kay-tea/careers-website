91cf9fd9d69c09e529550399436330bb12573c06
kay-tea <118791706+kay-tea@users.noreply.github.com>
2023-07-10 13:43:43 +0000

    improved jobpage layout, added conditionals for unfilled table data, implimented homepage in navbar

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_all_jobs_from_db, load_single_job_from_db
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  jobs_list = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs_list, company_name="Katies")
+  jobs_list = load_all_jobs_from_db()
+  return render_template('home.html', jobs=jobs_list)
 
+@app.route("/jobs/<id>")
+def show_job(id):
+  job_list = load_single_job_from_db(id)
+  if not job_list:
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job_list)
 
 @app.route("/api/jobs")
 def list_jobs():
-  jobs_list = load_jobs_from_db()
+  jobs_list = load_all_jobs_from_db()
   return jsonify(jobs_list)
 
 
--- database.py
+++ database.py
@@ -1,16 +1,26 @@
 from sqlalchemy import create_engine, text
 import os
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string = "mysql+pymysql://gyr2v40l0go1akr1f6ln:pscale_pw_ntmSj9o45JG80ADVWVDb3uev951aBER8XEO7Qa31J9M@aws.connect.psdb.cloud/careers?charset=utf8mb4"
+# db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string, connect_args={'ssl': {'ssl-ca': '/etc/ssl/cert.pem'}})
 
 
-def load_jobs_from_db():
+def load_all_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
     jobs = []
     for row in result.all():
       jobs.append(dict(row._mapping))
     return jobs
+
+def load_single_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id = :val"), {"val": id})
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
 
